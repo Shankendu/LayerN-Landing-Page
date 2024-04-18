6218a718c0af592aa1f8d8ffd39b12147bb9aa23
ShankenduK <shankendu@gmail.com>
2024-04-18 21:29:53 +0530

    Ui change

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -17,14 +17,6 @@ export default function Dropdown(props) {
             >
               Launch 0xSwap
             </a>
-
-            <a
-              href="#"
-              className="block rounded-lg px-4 hover:translate-y-1 py-2 text-sm hover:bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-teal-500 via-purple-500 to-green-200 transition-transform ease-in-out duration-150 text-white font-poppins font-medium"
-              role="menuitem"
-            >
-              Launch zk-client
-            </a>
           </div>
         </div>
       </div>
